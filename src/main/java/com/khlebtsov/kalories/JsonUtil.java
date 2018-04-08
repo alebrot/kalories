0c7d677f66e3da8daad1fa21cb85c6e604898688
REPLYNET\a.khlebtsov <a.khlebtsov@reply.it>
2018-04-08 18:20:10 +0200

    Add Json Util

---
+++ src/main/java/com/khlebtsov/kalories/JsonUtil.java
@@ -0,0 +1,75 @@
+package com.khlebtsov.kalories;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class JsonUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(JsonUtil.class);
+
+    private static ObjectMapper mapperToJsonInit() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+        SimpleFilterProvider simpleFilterProvider = new SimpleFilterProvider();
+        simpleFilterProvider.setFailOnUnknownId(false);
+        mapper.setFilterProvider(simpleFilterProvider);
+        return mapper;
+    }
+
+    private static ObjectMapper mapperToObjectInit() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+
+        SimpleFilterProvider simpleFilterProvider = new SimpleFilterProvider();
+        simpleFilterProvider.setFailOnUnknownId(false);
+        mapper.setFilterProvider(simpleFilterProvider);
+        mapper.registerModule(new Jdk8Module());
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+
+    private static ObjectMapper mapperToJson = mapperToJsonInit();
+    private static ObjectMapper mapperToObject = mapperToObjectInit();
+
+
+    private JsonUtil() {
+    }
+
+
+    public static String toJson(Object object) {
+
+        String s = null;
+        try {
+            s = mapperToJson.writeValueAsString(object);
+        } catch (IOException e) {
+            logger.error("Can not convert to json: {}", e);
+        }
+        return s;
+    }
+
+    public static <T> T toObject(String jsonInString, Class<T> aClass) {
+
+        T object = null;
+        try {
+            object = mapperToObject.readValue(jsonInString.getBytes(), aClass);
+        } catch (IOException e) {
+            logger.error("Can not construct object from json {}", e);
+        }
+        return object;
+    }
+}
--- src/test/java/com/khlebtsov/kalories/KaloriesApplicationTests.java
+++ src/test/java/com/khlebtsov/kalories/KaloriesApplicationTests.java
@@ -1,16 +1,53 @@
 package com.khlebtsov.kalories;
 
+import com.khlebtsov.kalories.entity.Meal;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KaloriesApplicationTests {
 
+    public static final String URL_GO = "/go";
+
+    @Autowired
+    protected WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.webApplicationContext);
+        mockMvc = builder.build();
+    }
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws Exception {
+
+
+        MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.get(URL_GO)
+                        .contentType(MediaType.APPLICATION_JSON);
+        ResultActions resultActions = this.mockMvc.perform(builder);
+        MvcResult mvcResult = resultActions.andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String contentAsString = response.getContentAsString();
+
+        Meal[] meals = JsonUtil.toObject(contentAsString, Meal[].class);
     }
 
 }
