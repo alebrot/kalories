6601e400844fb2a0a74ba9eb51eda962c032dd72
REPLYNET\a.khlebtsov <a.khlebtsov@reply.it>
2018-04-09 18:30:20 +0200

    add transaction manager

---
+++ src/main/java/com/khlebtsov/kalories/TransactionManagersConfig.java
@@ -0,0 +1,24 @@
+package com.khlebtsov.kalories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionManagersConfig {
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager(@Autowired EntityManagerFactory entityManagerFactory, @Autowired DataSource dataSource) {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
+        jpaTransactionManager.setDataSource(dataSource);
+        return jpaTransactionManager;
+    }
+}
